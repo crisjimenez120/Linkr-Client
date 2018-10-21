734ec7ad6acece3fe91e7f96e1952c2686963840
Cristopher Jimenez <crisjimenez120@gmail.com>
2018-10-21 15:10:09 -0400

    Changed the colors of some events

--- src/Components/Calendar/Calendar.js
+++ src/Components/Calendar/Calendar.js
@@ -11,20 +11,50 @@ class myCalendar extends Component {
       {
         start: new Date(),
         end: new Date(moment().add(1, "days")),
-        title: "Some title"
+        title: "1 title"
+
+      },
+      {
+        start: new Date(),
+        end: new Date(moment().add(5, "days")),
+        title: "5 title"
+      },
+      {
+        start: new Date(),
+        end: new Date(moment().add(3, "days")),
+        title: "3 title",
+        hexColor: '90ee90'
       }
     ]
   };
+  eventStyleGetter = (event, start, end, isSelected) =>{
+    console.log(event);
+    var backgroundColor = '#' + event.hexColor;
+    var style = {
+        backgroundColor: backgroundColor,
+        borderRadius: '0px',
+        opacity: 0.8,
+        color: 'black',
+        border: '0px',
+        display: 'block'
+    };
+    return {
+        style: style
+    };
+	}
+
 
   render() {
     return (
-      <div className="App">
+      <div className="App" >
         <Calendar
           localizer={localizer}
           defaultDate={new Date()}
           defaultView="month"
           events={this.state.events}
-          style={{ height: "90vh", width: "50vw"}}
+          style={{ height: "90vh", width: "55vw"}}
+          eventPropGetter={(this.eventStyleGetter)}
+  		  
         />
       </div>
     );
